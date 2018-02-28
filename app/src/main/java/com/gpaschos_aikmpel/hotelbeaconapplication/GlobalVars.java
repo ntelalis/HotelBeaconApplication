1844933cbc572817fa03bc3b024ff2461b60a454
gpaschos91 <gpaschos91@gmail.com>
2018-02-28 20:29:17 +0200

    forgotActivity + ServerFunctions + RequestQueueVolley + LoginChanges

---
+++ app/src/main/java/com/gpaschos_aikmpel/hotelbeaconapplication/GlobalVars.java
@@ -0,0 +1,14 @@
+package com.gpaschos_aikmpel.hotelbeaconapplication;
+
+/**
+ * Created by gpaschos on 28/02/18.
+ */
+
+public class GlobalVars {
+
+    private static final String ServerAddress = "http://192.168.1.101/";
+    private static final String loginPage = "login.php";
+    private static final String forgotPage = "forgot.php";
+    public static final String loginUrl = ServerAddress + loginPage;
+    public static final String forgotUrl = ServerAddress + forgotPage;
+}
