96af5ac654165a395e0387c9edf3e3ec993657c0
gpaschos91 <gpaschos91@gmail.com>
2018-03-16 17:24:38 +0200

    book and bookconfirm connection

--- app/src/main/java/com/gpaschos_aikmpel/hotelbeaconapplication/BookActivity.java
+++ app/src/main/java/com/gpaschos_aikmpel/hotelbeaconapplication/BookActivity.java
@@ -1,14 +1,21 @@
 package com.gpaschos_aikmpel.hotelbeaconapplication;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class BookActivity extends AppCompatActivity {
+import com.gpaschos_aikmpel.hotelbeaconapplication.RequestVolley.JsonListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class BookActivity extends AppCompatActivity implements JsonListener{
     public static final String ROOMTITLE = "lala";
     public static final String ROOMPRICE = "leprice";
     public static final String ROOMIMAGE = "image";
@@ -50,6 +57,23 @@ public class BookActivity extends AppCompatActivity {
             //Toast.makeText(this, roomTitle+"\n"+roomPrice+"\n"+arrival+"\n"+
               //      departure+"\n"+persons+"\n", Toast.LENGTH_SHORT).show();
         }
+
+    }
+
+    public void confirmAndBook(View view){
+
+    }
+
+    @Override
+    public void getSuccessResult(String url, JSONObject json) throws JSONException {
+        int resID = json.getInt("ReservationID");
+        Intent intent = new Intent(this,BookConfirmation.class);
+        intent.putExtra(BookConfirmation.RESERVATION_NUMBER_KEY,resID);
+        startActivity(intent);
+    }
+
+    @Override
+    public void getErrorResult(String url, String error) {
 
     }
 }
