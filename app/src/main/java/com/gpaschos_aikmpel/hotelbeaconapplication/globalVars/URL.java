38d2aafba39b8b5f96a281020e468fa14be933c1
gpaschos91 <gpaschos91@gmail.com>
2018-03-22 16:12:48 +0200

    local changes and viewpager changes

--- app/src/main/java/com/gpaschos_aikmpel/hotelbeaconapplication/globalVars/URL.java
+++ app/src/main/java/com/gpaschos_aikmpel/hotelbeaconapplication/globalVars/URL.java
@@ -4,11 +4,11 @@ public class URL {
 
     private static final String serverProtocol = "http";
 
-    private static final String serverHost = "ntelalis.dyndns-home.com";
-    //private static final String serverHost = "192.168.1.101";
+    //private static final String serverHost = "ntelalis.dyndns-home.com";
+    private static final String serverHost = "192.168.1.101";
 
-    private static final String serverPort = "8642";
-    //private static final String serverPort = "80";
+    //private static final String serverPort = "8642";
+    private static final String serverPort = "80";
 
     private static final String serverAddress = serverProtocol + "://" + serverHost + ":" + serverPort + "/";
 
