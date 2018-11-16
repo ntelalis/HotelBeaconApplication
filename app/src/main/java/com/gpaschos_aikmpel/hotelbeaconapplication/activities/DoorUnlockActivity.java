42ccabc976d1915e2a4e4d7794c0488de9b4511f
Aikaterini Mpelmezou <cathm247@yahoo.com>
2018-11-16 14:17:33 +0200

    commit after merge with new scanning times

--- app/src/main/java/com/gpaschos_aikmpel/hotelbeaconapplication/activities/DoorUnlockActivity.java
+++ app/src/main/java/com/gpaschos_aikmpel/hotelbeaconapplication/activities/DoorUnlockActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
